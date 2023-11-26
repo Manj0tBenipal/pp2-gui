4d977f75f11039b514172fe8b436de0253ba3a5c
Manj0tBenipal <manjotbenipal00001@gmail.com>
2023-11-26 16:41:42 -0500

    Select box supporting mutiple insertions is supported now

--- components/AddNewMovie.tsx
+++ components/AddNewMovie.tsx
@@ -1,6 +1,14 @@
 "use client";
 import styles from "@/components/newmovie.module.css";
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import {
+  Checkbox,
+  FormControl,
+  InputLabel,
+  ListItemText,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
 import CancelIcon from "@mui/icons-material/Cancel";
 import {
   Character,
@@ -9,6 +17,7 @@ import {
   GenresMovie,
   Movie,
 } from "@/app/types/Movie";
+import { useState } from "react";
 
 export default function AddNewMovie({
   setVisibility,
@@ -29,6 +38,10 @@ export default function AddNewMovie({
   setMovies: Function;
   setGenresMovie: Function;
 }) {
+  const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
+  const [selectedCountry, setSelectedCountry] = useState<string>();
+  const [selectedCharacters, setSelectedCharacter] = useState<string[]>([]);
+  console.log(selectedCharacters);
   return (
     <div className={`${styles.wrapper}`}>
       <div className={`${styles.container}`}>
@@ -41,18 +54,86 @@ export default function AddNewMovie({
             cursor: "pointer",
           }}
         />
-        <FormControl>
-          <InputLabel id="demo-simple-select-label">Age</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            label="Genre"
-          >
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-          </Select>
-        </FormControl>
+        <form className={`${styles.form} flex flex-even`}>
+          <FormControl fullWidth>
+            <InputLabel id="genre">Genre</InputLabel>
+            <Select labelId="genre" id="genreSelect" label="Genre">
+              {genres.map((genre: Genre) => {
+                return (
+                  <MenuItem value={genre.genreId}>{genre.genreName}</MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+          <FormControl fullWidth>
+            <InputLabel id="countryLabel">Country</InputLabel>
+            <Select labelId="counrtyLabel" id="countrySelect" label="Country">
+              {countries.map((country: Country) => {
+                return (
+                  <MenuItem value={country.countryId}>
+                    {country.countryName}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+          <FormControl fullWidth>
+            <InputLabel id="characterLabel">Characters</InputLabel>
+            <Select
+              labelId="characterLabel"
+              id="characterSelect"
+              label="Character"
+              multiple
+              onChange={(e: SelectChangeEvent<typeof selectedCharacters>) =>
+                setSelectedCharacter(
+                  typeof e.target.value === "string"
+                    ? e.target.value.split(",")
+                    : e.target.value
+                )
+              }
+              renderValue={(selectedCharacters) => {
+                let string = "";
+                const c: any = selectedCharacters.map((id: number | string) => {
+                  const formattedId: number =
+                    typeof id === "string" ? parseInt(id) : id;
+                  return characters.find(
+                    (el: Character) => el.characterId === formattedId
+                  );
+                });
+                c.map((el: Character) => {
+                  string += el.characterName + "(" + el.actorName + ") ,";
+                });
+                return string;
+              }}
+              value={selectedCharacters}
+            >
+              {characters.map((character: Character) => {
+                return (
+                  <MenuItem
+                    key={character.characterId}
+                    value={character.characterId}
+                  >
+                    <Checkbox
+                      checked={
+                        selectedCharacters.findIndex(
+                          (c: string) => character.characterId === parseInt(c)
+                        ) > -1
+                      }
+                    />
+                    <ListItemText
+                      primary={
+                        character.characterName +
+                        "(" +
+                        character.actorName +
+                        ")"
+                      }
+                    />
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+        </form>
       </div>
     </div>
   );
